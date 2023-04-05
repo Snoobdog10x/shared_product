aab6fe17f96970a3696b58e339be3b6f397aa594
Nguyen Duy Thanh <duythanh1565@gmail.com>
2023-04-05 14:15:07 +0700

    add shared text and remove debug print

--- services/security.dart
+++ services/security.dart
@@ -6,7 +6,6 @@ class Security {
   String hashPassword(String inputPassword) {
     var bytes = utf8.encode(inputPassword);
     var digest = sha256.convert(bytes);
-    print(digest);
     return digest.toString();
   }
 }
---
+++ utils/shared_text_style.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SharedTextStyle {
+  //Size
+  static double TITLE_SIZE = 20;
+  static double SUB_TITLE_SIZE = 16;
+  static double NORMAL_SIZE = 14;
+
+  //Fontweight
+  static FontWeight TITLE_WEIGHT = FontWeight.w700;
+  static FontWeight SUB_TITLE_WEIGHT = FontWeight.w600;
+  static FontWeight NORMAL_WEIGHT = FontWeight.w400;
+
+  //FontColor
+  static String DEFAULT_FONT_TITLE = '.SF Pro Display';
+  static String DEFAULT_FONT_TEXT = '.SF Pro Text';
+}
