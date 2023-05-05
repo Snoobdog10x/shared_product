10123674d34c4b4e66c00b81a1f85b86bf43428e
Nguyen Duy Thanh <duythanh1565@gmail.com>
2023-05-05 13:50:11 +0700

    add priority set

---
+++ vendors/priority_set/priority_set.dart
@@ -0,0 +1,31 @@
+import 'package:collection/collection.dart';
+
+class PrioritySet<T> {
+  List<T> _data = [];
+  late int Function(T, T)? comparison;
+  PrioritySet(int Function(T, T)? comparison) {
+    this.comparison = comparison;
+  }
+
+  void add(T item) {
+    _data.remove(item);
+    _data.add(item);
+    _data.sort(comparison);
+  }
+
+  void addAll(Iterable<T> objectList) {
+    objectList.forEach((object) {
+      _data.remove(object);
+      _data.add(object);
+    });
+    _data.sort(comparison);
+  }
+
+  void clear() {
+    _data.clear();
+  }
+
+  List<T> toList() {
+    return _data.toList();
+  }
+}
