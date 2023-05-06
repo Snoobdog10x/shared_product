f29e5e8c886944994c55bec217d9746a575e01ce
Nguyen Duy Thanh <duythanh1565@gmail.com>
2023-05-07 00:22:17 +0700

    refactor isSameWeek

--- utils/format/format_utlity.dart
+++ utils/format/format_utlity.dart
@@ -10,11 +10,16 @@ class FormatUtility {
   }
 
   static bool isSameWeek(DateTime date1, DateTime date2) {
+    DateTime date1AddWeek =
+        DateTime(date1.millisecondsSinceEpoch).add(Duration(days: 6));
+    ;
+    DateTime date2AddWeek =
+        DateTime(date2.millisecondsSinceEpoch).add(Duration(days: 6));
     int weekNumber1 = date1.weekday == DateTime.sunday
-        ? date1.difference(Duration(days: 6) as DateTime).inDays ~/ 7
+        ? date1.difference(date1AddWeek).inDays ~/ 7
         : date1.weekday - 1;
     int weekNumber2 = date2.weekday == DateTime.sunday
-        ? date2.difference(Duration(days: 6) as DateTime).inDays ~/ 7
+        ? date2.difference(date2AddWeek).inDays ~/ 7
         : date2.weekday - 1;
     return weekNumber1 == weekNumber2 && date1.year == date2.year;
   }
