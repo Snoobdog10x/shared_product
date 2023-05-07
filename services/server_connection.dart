993e5c53fc282fab84bbbcdc10acb49338ec13da
Nguyen Duy Thanh <duythanh1565@gmail.com>
2023-05-07 12:08:14 +0700

    update connection

--- services/server_connection.dart
+++ services/server_connection.dart
@@ -2,18 +2,26 @@ import 'dart:async';
 
 import 'package:firebase_database/firebase_database.dart';
 import 'package:reel_t/generated/abstract_service.dart';
+import 'package:reel_t/generated/app_init.dart';
+import 'package:reel_t/generated/app_store.dart';
 
 class ServerConnection extends AbstractService {
   StreamSubscription<DatabaseEvent>? _connectionStream;
   final realtimeDatabase = FirebaseDatabase.instance;
+  AppStore _appStore = AppInit.appStore;
+  void Function(bool isConnected)? _connectionChangeCallBack;
+  void setCallBack(void Function(bool isConnected) _connectionChangeCallBack) {
+    this._connectionChangeCallBack = _connectionChangeCallBack;
+  }
+
   void init() {
-    _connectionStream =
-        realtimeDatabase.ref(".info/connected").onValue.listen((event) {
-      if (event.snapshot.value != null) {
-        print("connected");
-        return;
-      }
-      print("disconnected");
+    _connectionStream = realtimeDatabase
+        .ref(".info")
+        .child("connected")
+        .onValue
+        .listen((event) {
+      var isConnected = event.snapshot.value as bool;
+      _connectionChangeCallBack?.call(isConnected);
     });
   }
 
